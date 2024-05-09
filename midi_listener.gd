10be16a15df2b6e0a110a72c4a56282378b9af43
Jonng <jonngkh@gmail.com>
2024-05-09 22:28:12 +0800

    listens for midi C4 on and off

--- midi_listener.gd
+++ midi_listener.gd
@@ -3,7 +3,22 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	OS.open_midi_inputs()
+	print(OS.get_connected_midi_inputs())
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	
+	if event is InputEventMIDI:
+		var note_on: bool = (event.message == 9)
+		var note_off: bool = (event.message == 8)
+		
+		if note_on:
+			if event.pitch == 60:
+				print('C4 on')
+		if note_off:
+			if event.pitch == 60:
+				print('C4 off')
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
