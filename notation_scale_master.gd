9b9bf3c326639380011c2ca2d44e909bad88dbf5
Jonng <jonngkh@gmail.com>
2024-07-08 21:45:07 +0800

    planned future steps as comments

--- notation_scale_master.gd
+++ notation_scale_master.gd
@@ -1,1 +1,11 @@
 extends Control
+
+# same UI as flat.io
+# all rests, mouse over a position to show a colored note. click to place it
+# click and drag existing note to move it (only vertically)
+# click time sign and get options to change it
+# changes number of rests and distribution of notes on bars
+# save button reads the composition and writes the notes and positions to an array and writes to a file
+# need to name it
+# page shows all the compositions by reading from the file
+# import midi or musicxml or mxl or musescore mscz mscx
