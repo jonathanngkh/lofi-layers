edc0dfeec0df757522a9a555aa0f111b1c6d862b
Jonng <jonngkh@gmail.com>
2024-07-11 10:52:52 +0800

    working click to paste note and double click to remove note notation system. next: play sound on paste, on remove, press play to play from left to right, save to external file, rests there by default, note tails

--- note_head.gd
+++ note_head.gd
@@ -1,5 +1,7 @@
-extends TextureButton
+extends TextureRect
 
+var hover := false
+var placed := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,15 +9,40 @@ func _ready() -> void:
 	modulate.a = 0.0
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
+	for child in get_children():
+		for grandchild in child.get_children():
+			grandchild.mouse_filter = MOUSE_FILTER_IGNORE
 
 
 func _on_mouse_entered() -> void:
-	modulate.a = 1.0
+	hover = true
+	if placed == false:
+		modulate.a = 1.0
 
 
 func _on_mouse_exited() -> void:
-	modulate.a = 0.0
+	hover = false
+	if placed == false:
+		modulate.a = 0.0
 
+
+func _input(event: InputEvent) -> void:
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.pressed
+	)
+	var event_is_mouse_double_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.double_click
+	)
+	if event_is_mouse_click and hover == true and placed == false:
+		placed = true
+		modulate.a = 1.0
+	if event_is_mouse_double_click and hover == true and placed == true:
+		placed = false
+		modulate.a = 0.0
 
 # default: invisible
 # hover: visible but dimmed or blinking or something to indicate not confirmed
