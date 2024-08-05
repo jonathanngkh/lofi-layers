64c4370540ee9c646e4ef926f2f9aaa97c81c738
Jonng <jonngkh@gmail.com>
2024-08-05 21:35:35 +0800

    fixed running backwards glitch on warrior run

--- Characters/warrior/run.gd
+++ Characters/warrior/run.gd
@@ -3,6 +3,7 @@ extends WarriorState
 
 # Called by the state machine upon changing the active state. The `msg` parameter is a dictionary with arbitrary data the state can use to initialize itself.
 func enter(_msg := {}) -> void:
+	warrior.sprite.scale.x = 1
 	if _msg["direction"] == "left":
 		warrior.sprite.play("run_left")
 	elif _msg["direction"] == "right":
@@ -28,7 +29,6 @@ func physics_update(_delta: float) -> void:
 ## Receives events from the `_unhandled_input()` callback.
 func handle_input(_event: InputEvent) -> void:
 	if Input.get_axis("left", "right") > 0:
-		warrior.sprite.scale.x = 1
 		warrior.velocity.x = 1 * warrior.SPEED
 	elif Input.get_axis("left", "right") < 0:
 		warrior.velocity.x = -1 * warrior.SPEED
