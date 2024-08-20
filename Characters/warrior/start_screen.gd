bea6ad2bea9a660c2af45e145faf5b27d20afae6
Jonng <jonngkh@gmail.com>
2024-08-21 01:44:15 +0800

    start screen and export settings for windows

---
+++ Characters/warrior/start_screen.gd
@@ -0,0 +1,17 @@
+extends ColorRect
+
+@onready var game_started = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("light_attack"):
+		if game_started == false:
+			game_started = true
+			$AudioStreamPlayer.play()
+			var tween = create_tween()
+			tween.tween_property(self, "modulate:a", 0, 0.5)
+	
