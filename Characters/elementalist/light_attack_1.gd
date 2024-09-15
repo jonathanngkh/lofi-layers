6dfcd29b6a95f4f3e9e6cd20cc0f133b9cd2f457
Jonng <jonngkh@gmail.com>
2024-09-15 23:40:25 +0800

    earth projectile rises from ground then flies forward then transitions to impact animation

--- Characters/elementalist/light_attack_1.gd
+++ Characters/elementalist/light_attack_1.gd
@@ -50,10 +50,13 @@ func _on_frame_changed() -> void:
 		var earth_projectile_instance := earth_projectile_preload.instantiate()
 		# remember to code when facing left also
 		earth_projectile_instance.scale = elementalist.scale
-		earth_projectile_instance.global_position = elementalist.global_position + Vector2(80, -100)
+		earth_projectile_instance.global_position = elementalist.global_position + Vector2(80, 0)
 		var tween = create_tween()
+		tween.tween_property(earth_projectile_instance, "global_position:y", earth_projectile_instance.global_position.y - 90, 0.2)
 		tween.tween_property(earth_projectile_instance, "global_position:x", earth_projectile_instance.global_position.x + 1000, 0.4)
 		elementalist.get_parent().add_child(earth_projectile_instance)
+		await tween.finished
+		earth_projectile_instance.play("impact", 1.5)
 		pass
 	if elementalist.sprite.frame == 1:
 		pass
--- utility/earth_projectile.gd
+++ utility/earth_projectile.gd
@@ -3,6 +3,7 @@ extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	animation = "projectile"
 	animation_finished.connect(_on_animation_finished)
 
 
