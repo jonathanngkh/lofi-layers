b2d3512433977987857d2710cd1afb8960d04c4d
Jonng <jonngkh@gmail.com>
2024-09-23 23:41:09 +0800

    autobattler ep 2

---
+++ utility/AutoBattler/unit.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+@export var stats : UnitStats : set = set_stats
+
+@onready var skin : Sprite2D = $Skin
+@onready var health_bar : ProgressBar = $HealthBar
+@onready var mana_bar : ProgressBar = $ManaBar
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func set_stats(value: UnitStats) -> void:
+	stats = value
+	
+	if value == null:
+		return
+	
+	if not is_node_ready():
+		await ready
+	
+	skin.region_rect.position = Vector2(stats.skin_coordinates) * Arena.CELL_SIZE
